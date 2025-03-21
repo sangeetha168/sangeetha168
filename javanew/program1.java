ac28dbae2bde6b31fa0e58f9078120b05a4fcf3f
sangeetha_2025 <sangeethasupritha2@gmail.com>
2025-03-21 23:37:03 +0530

    intial commit

---
+++ javanew/program1.java
@@ -0,0 +1,14 @@
+public class program1 {
+    public static void main(String[] args) {
+        byte bt=126;
+        System.out.println("byte value="+bt);
+        short st =bt;
+        System.out.println("short value="+st);
+        int it=st;
+        System.out.println("int value="+it);
+        long lg=it;
+        System.out.println("long value ="+lg);
+
+    }
+
+}
---
+++ javanew/program2.java
@@ -0,0 +1,12 @@
+public class program2 {
+public static void main(String[] args) {
+    long lg=126;
+    System.out.println("long value="+lg);
+    int it=(int)lg;
+    System.out.println("int value="+it);
+    short st=(short)it;
+    System.out.println("short value="+st);
+    byte bt=(byte)st;
+    System.out.println("byte value="+bt);
+}
+}
